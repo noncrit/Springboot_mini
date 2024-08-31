187382ddc2e117e992aa3deb474614b7729c0f23
Seokjin <kimksj09996@gmail.com>
2024-08-31 23:29:49 +0900

    Builded Service Logic

---
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원 가입
+    //단, 같은 이름의 회원은 가입 불가
+    public Long join(Member member) {
+        //이름 중복 회원 가입 불가
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m->{
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        //조금 더 권장되는 형태(함수형 프로그래밍 형태)
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
